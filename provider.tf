a6d43a5c73d2c1359d74c447acc3fc392342c160
mad5bo <68882369+mad5bo@users.noreply.github.com>
2020-09-10 11:37:02 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
+#####################################################################
+##
+##      Created 9/8/20 by IBMDemo.
+##
+#####################################################################
 provider "ibm" {
-    version          = "~> 0.3"
     ibmcloud_api_key = "${var.ibmcloud_api_key}"
     region           = "${var.ibmcloud_region}"
 #    zone             = "${var.ibmcloud_zone}"
