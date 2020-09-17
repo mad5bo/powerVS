c51c79a9dc3f29f5a44eb690d63bf113f25a6a69
mad5bo <68882369+mad5bo@users.noreply.github.com>
2020-09-17 10:12:44 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-#data "ibm_pi_network" "power_networks" {
-#    count                = "${length(var.networks)}"
-#    pi_network_name      = "${var.networks[count.index]}"
-#    pi_cloud_instance_id = "${var.power_instance_id}"
-#}
-
-data "ibm_pi_image" "power_images" {
+data "ibm_pi_image" "PowerVS_images" {
     pi_image_name        = "${var.image_name}"
-#    pi_image_name        = "ibmi73vm"
     pi_cloud_instance_id = "${var.power_instance_id}"
 }
 
-resource "ibm_pi_network" "power_networks" {
+resource "ibm_pi_network" "PowerVS_networks" {
   count                = 1
   pi_network_name      = "${var.networkname}"
   pi_cloud_instance_id = "${var.power_instance_id}"
@@ -18,28 +11,22 @@ resource "ibm_pi_network" "power_networks" {
 }
 
 data "ibm_pi_public_network" "dsnetwork" {
-  depends_on           = ["ibm_pi_network.power_networks"]
+#  depends_on           = ["ibm_pi_network.PowerVS_networks"]
   pi_cloud_instance_id = "${var.power_instance_id}"
 }
 
-resource "ibm_pi_instance" "pvminstance" {
+resource "ibm_pi_instance" "PowerVS_instance" {
     pi_memory             = "${var.memory}"
     pi_processors         = "${var.processors}"
     pi_instance_name      = "${var.vm_name}"
     pi_proc_type          = "${var.proc_type}"
-#    pi_migratable         = "${var.migratable}"
-    pi_image_id           = "${data.ibm_pi_image.power_images.id}"
-#    pi_image_id           = "${data.ibm_pi_image.power_images.imageid}"
-#    pi_image_id           = "${data.ibm_pi_image.power_images.imageid}"
-    pi_volume_ids         = []
-#    pi_network_ids        = ["${data.ibm_pi_network.power_networks.*.networkid}"]
+#   pi_migratable         = "${var.migratable}"
+    pi_image_id           = "${data.ibm_pi_image.PowerVS_images.id}"
+#   pi_volume_ids         = []
     pi_network_ids        = ["${data.ibm_pi_public_network.dsnetwork.id}"]
     pi_key_pair_name      = "${var.ssh_key_name}"
     pi_sys_type           = "${var.system_type}"
-    pi_replication_policy = "${var.replication_policy}"
-#    pi_replication_scheme = "${var.replication_scheme}"
+#   pi_replication_policy = "${var.replication_policy}"
     pi_replicants         = "${var.replicants}"
     pi_cloud_instance_id  = "${var.power_instance_id}"
-#    pi_cloud_instance_id  = "30295a9a-9ffa-4b5b-8b7d-efa06f3d38a7"
-
 }
